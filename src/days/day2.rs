166efad013fa3534d05851406b1adfe7cb3399eb
Zach Combs <zacharycombstwo@gmail.com>
2023-12-04 15:02:35 -0500

    Finished day 2 part 2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{self, BufRead},
     path::Path,
@@ -15,6 +16,70 @@ pub fn find_sum_of_ids() {
     }
 
     println!("Sum: {}", valid_game_id_total);
+}
+
+pub fn find_power_of_minimum_cubes_per_game() {
+    let mut total_sum_of_powers: i32 = 0;
+    if let Ok(lines) = read_lines("inputs/day_2_input.txt") {
+        for line in lines {
+            if let Ok(line) = line {
+                total_sum_of_powers += find_minimum_num_of_blocks(&line);
+            }
+        }
+    }
+
+    println!("Sum: {}", total_sum_of_powers);
+}
+
+fn find_minimum_num_of_blocks(game: &String) -> i32 {
+    let game_data: Vec<&str> = game.split(":").collect();
+
+    let mut min_block_hashmap: HashMap<&str, i32> = HashMap::new();
+
+    for reveal in game_data[1].split(";") {
+        for cubes_by_color in reveal.split(",") {
+            let number_of_cube_of_color: Vec<&str> = cubes_by_color.trim().split(" ").collect();
+            // if possible_game_restraints.get(number_of_cube_of_color[1])
+            //     < number_of_cube_of_color[0].parse::<i32>().as_ref().ok()
+            // {
+            //     return 0;
+            // }
+            match min_block_hashmap.get(number_of_cube_of_color[1]) {
+                Some(value) => {
+                    if value
+                        < number_of_cube_of_color[0]
+                            .parse::<i32>()
+                            .as_ref()
+                            .ok()
+                            .unwrap()
+                    {
+                        min_block_hashmap.insert(
+                            number_of_cube_of_color[1],
+                            number_of_cube_of_color[0].parse::<i32>().ok().unwrap(),
+                        );
+                    }
+                }
+                None => {
+                    min_block_hashmap.insert(
+                        number_of_cube_of_color[1],
+                        number_of_cube_of_color[0].parse::<i32>().ok().unwrap(),
+                    );
+                }
+            }
+        }
+    }
+
+    let mut hashmap_set_power = 1;
+
+    for value in min_block_hashmap.values() {
+        if hashmap_set_power == 0 {
+            hashmap_set_power = *value;
+        } else {
+            hashmap_set_power *= value;
+        }
+    }
+
+    return hashmap_set_power;
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 mod days;
 
 use days::day1::find_sum_of_numbers_and_letters;
-use days::day2::find_sum_of_ids;
+use days::day2::{find_power_of_minimum_cubes_per_game, find_sum_of_ids};
 
 #[macro_use]
 extern crate maplit;
 fn main() {
-    print!("Here is the output for day 1 -> ");
+    print!("Here is the output for day 1 part 2 -> ");
     find_sum_of_numbers_and_letters();
 
-    print!("Here is the outpur for day 2 -> ");
-    find_sum_of_ids()
+    print!("Here is the output for day 2 part 1 -> ");
+    find_sum_of_ids();
+
+    print!("Here is the output for day 2 part 2 -> ");
+    find_power_of_minimum_cubes_per_game();
 }

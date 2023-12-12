b5ee7b3087a366a8c3349c3f2300df2820023024
Zach Combs <zacharycombstwo@gmail.com>
2023-12-12 11:00:46 -0500

    Finish day 4

--- src/days/day11_part_2.rs
+++ src/days/day11_part_2.rs
@@ -15,23 +15,28 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn expand_universe_on_empty_column_or_row(universe: Vec<Vec<String>>) -> Vec<Vec<String>> {
-    let mut new_universe: Vec<Vec<String>> = Vec::new();
+fn expand_universe_on_empty_column_or_row(
+    universe: Vec<Vec<String>>,
+) -> (Vec<Vec<String>>, HashMap<i32, &'static str>) {
+    let new_universe: Vec<Vec<String>> = universe.clone();
+    let mut map_of_large_expansions: HashMap<i32, &str> = HashMap::new();
 
-    for row in universe.into_iter().rev() {
-        new_universe.insert(0, row.clone());
+    for (index, row) in universe.into_iter().enumerate() {
         if row.iter().all(|point| point == ".") {
-            new_universe.insert(0, vec!["!".to_string(); row.len()]);
+            // if let Some(empty_row) = new_universe.get_mut(index) {
+            //     *empty_row = vec!["!".to_string(); row.len()]
+            // }
+            map_of_large_expansions.insert(index as i32, "row");
         }
     }
-    for row in &new_universe {
-        for element in row {
-            print!("{} ", element);
-        }
-        println!(); // Move to the next line after printing each row
-    }
+    // for row in &new_universe {
+    //     for element in row {
+    //         print!("{} ", element);
+    //     }
+    //     println!(); // Move to the next line after printing each row
+    // }
 
-    let mut transposed_universe: Vec<Vec<_>> = (0..new_universe[0].len())
+    let transposed_universe: Vec<Vec<_>> = (0..new_universe[0].len())
         .map(|col_index| {
             new_universe
                 .iter()
@@ -40,21 +45,29 @@ fn expand_universe_on_empty_column_or_row(universe: Vec<Vec<String>>) -> Vec<Vec
         })
         .collect();
 
-    for (index, row) in transposed_universe.clone().iter().enumerate().rev() {
-        if row.iter().all(|point| point == "." || point == "!") {
-            transposed_universe.insert(index, vec!["!".to_string(); row.len()]);
+    for (index, row) in transposed_universe.clone().iter().enumerate() {
+        if row.iter().all(|point| point == ".") {
+            // if let Some(empty_row) = new_universe.get_mut(index) {
+            //     *empty_row = vec!["!".to_string(); row.len()]
+            // }
+            map_of_large_expansions.insert(index as i32, "column");
         }
     }
     println!();
 
-    for row in &transposed_universe {
-        for element in row {
-            print!("{} ", element);
-        }
-        println!(); // Move to the next line after printing each row
-    }
+    // for row in &transposed_universe {
+    //     for element in row {
+    //         print!("{} ", element);
+    //     }
+    //     println!(); // Move to the next line after printing each row
+    // }
+    // println!();
 
-    return transposed_universe;
+    // for (key, value) in map_of_large_expansions {
+    //     println!("Key: {} Value: {}", key, value);
+    // }
+
+    return (new_universe, map_of_large_expansions);
 }
 
 fn convert_galaxy_to_number(universe: &Vec<Vec<String>>) -> Vec<Vec<String>> {
@@ -70,12 +83,26 @@ fn convert_galaxy_to_number(universe: &Vec<Vec<String>>) -> Vec<Vec<String>> {
         }
     }
 
+    // for row in &new_universe {
+    //     for element in row {
+    //         print!("{} ", element);
+    //     }
+    //     println!(); // Move to the next line after printing each row
+    // }
+
     return new_universe;
 }
 
-fn find_sum_of_paths_between_pairs(universe: Vec<Vec<String>>) {
+fn find_sum_of_paths_between_pairs(
+    universe: Vec<Vec<String>>,
+    map_of_gaps: HashMap<i32, &'static str>,
+) {
     let mut map_of_galaxy_coords: IndexMap<String, Vec<i32>> = IndexMap::new();
-    let mut sum_of_distances: i32 = 0;
+    let mut sum_of_distances: u128 = 0;
+
+    for (key, value) in &map_of_gaps {
+        println!("{} {}", key, value);
+    }
 
     for (x, row) in universe.iter().enumerate() {
         for (y, point) in row.iter().enumerate() {
@@ -90,22 +117,69 @@ fn find_sum_of_paths_between_pairs(universe: Vec<Vec<String>>) {
         let mut base_galaxy: Vec<i32> = vec![0, 0];
         let mut current_galaxy = "";
         while let Some((key, value)) = iter.next() {
+            // for row in &universe {
+            //     for element in row {
+            //         print!("{} ", element);
+            //     }
+            //     println!(); // Move to the next line after printing each row
+            // }
             if current_galaxy == "" {
                 base_galaxy = value.to_vec();
                 current_galaxy = key;
                 continue;
             }
-            let difference: i32 =
-                (base_galaxy[0] - (value[0])).abs() + (base_galaxy[1] - value[1]).abs();
-            sum_of_distances += difference;
+            let mut compounded_distance_x: u128 = 0;
+            let start_number_x = std::cmp::min(base_galaxy[0], value[0]);
+            let end_number_x = std::cmp::max(base_galaxy[0], value[0]);
+            // println!("Start Number: {}", start_number_x);
+            // println!("End Number: {}", end_number_x);
+            // println!("This is the base galaxy: {:?}", base_galaxy);
+
+            for num in start_number_x + 1..end_number_x {
+                if map_of_gaps
+                    .get(&num)
+                    .is_some_and(|orientation| *orientation == "row")
+                {
+                    // println!(
+                    //     "There is an empty row inbetween {} and {}",
+                    //     start_number_x, end_number_x
+                    // );
+                    compounded_distance_x += 999999;
+                }
+            }
+            // println!("Base Galaxy: {} {:?}", current_galaxy, base_galaxy);
+            // println!("Current Compare: {} {:?}", key, value);
+            // println!("Distance X: {}", compounded_distance_x);
+
+            let mut compounded_distance_y: u128 = 0;
+            let start_number_y = std::cmp::min(base_galaxy[1], value[1]);
+            let end_number_y = std::cmp::max(base_galaxy[1], value[1]);
+
+            for num in start_number_y + 1..end_number_y {
+                if map_of_gaps
+                    .get(&num)
+                    .is_some_and(|orientation| *orientation == "column")
+                {
+                    compounded_distance_y += 999999;
+                }
+            }
+            // println!("Distance Y: {}", compounded_distance_y);
+
+            let diff_x: u128 = (base_galaxy[0] - (value[0])).abs() as u128 + compounded_distance_x;
+            let diff_y: u128 = (base_galaxy[1] - value[1]).abs() as u128 + compounded_distance_y;
+            // println!("Distance X: {}", diff_x);
+            // println!("Distance Y: {}", diff_y);
+            sum_of_distances += diff_x + diff_y;
+            // println!("------------------------------------------")
         }
+        // println!("------------------------------");
     }
     println!("This is the sum: {}", sum_of_distances);
 }
 
 pub fn find_sum_of_shortest_path_of_pairs_1000000_bigger() {
     let mut universe: Vec<Vec<String>> = Vec::new();
-    if let Ok(lines) = read_lines("inputs/day_11_small_input.txt") {
+    if let Ok(lines) = read_lines("inputs/day_11_input.txt") {
         for line in lines {
             if let Ok(line) = line {
                 universe.push(line.chars().map(|c| c.to_string()).collect());
@@ -114,6 +188,6 @@ pub fn find_sum_of_shortest_path_of_pairs_1000000_bigger() {
     }
 
     let expanded_universe = expand_universe_on_empty_column_or_row(universe);
-    let numbafied_universe = convert_galaxy_to_number(&expanded_universe);
-    find_sum_of_paths_between_pairs(numbafied_universe);
+    let numbafied_universe = convert_galaxy_to_number(&expanded_universe.0);
+    find_sum_of_paths_between_pairs(numbafied_universe, expanded_universe.1);
 }
